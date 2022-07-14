e6ca14ab60b39032b788c808ca5e229608a6914e
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2022-07-14 10:17:16 +0700

    initail commit

--- source/function/const/get_file_from_sharepoint.py
+++ source/function/const/get_file_from_sharepoint.py
@@ -1,5 +1,3 @@
-
-
 import os
 
 # gcp
@@ -12,10 +10,8 @@ table_id = 'Exploration_Plan'
 # table -> Seismic_Plan
 table_id = 'Seismic_Plan'
 
-
 # sharepoint
-SERCRET = os.getenv('SERCRET', 'credentials\secret.json')
+SERCRET =  os.getenv('SERCRET', 'credentials\secret.json')
 server_url = "https://pttep.sharepoint.com/teams/SoftwareDevelopmentwithExternalParty"
 rel_path = "/teams/SoftwareDevelopmentwithExternalParty/Shared Documents/Digital PMO/99 Test Source Files/Digital_PMO_Test_Data.xlsx"
-
 # rel_path = "/teams/SoftwareDevelopmentwithExternalParty/Shared Documents/Digital PMO"
