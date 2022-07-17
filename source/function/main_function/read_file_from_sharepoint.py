3b01c4416e72782a2d08cf2cb54e3307e2c55844
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2022-07-17 23:07:45 +0700

    inital commit

--- source/function/main_function/read_file_from_sharepoint.py
+++ source/function/main_function/read_file_from_sharepoint.py
@@ -8,6 +8,7 @@ def read_file_from_sharepoint():
     
     df = pd.read_excel(files, sheet_name=0, skiprows=4, header=[0,1]).transpose().reset_index()
     data = df.iloc[[0,4] , 0:].reset_index().drop(['index','level_0', 'level_1'], axis=1) # get column 0 => Target Fields, 4=> Example Data
+    print(data)
     
     
     # for rows in data.itertuples(index=False):
